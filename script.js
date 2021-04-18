a4f1de455caaefa68377d66e2e3e66daf291b5df
Vraj Patel <56442375+vrajp3301@users.noreply.github.com>
2021-04-18 11:04:56 +0530

    Added title icon

--- script.js
+++ script.js
@@ -106,3 +106,50 @@ audio.addEventListener('timeupdate', updateProgress);
 progressContainer.addEventListener('click', setProgress);
 
 audio.addEventListener('ended', nextSong);
+
+// Night Sky element
+
+const $el = document.body;
+
+// Generate a random number between min and max values
+
+const genRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min;
+}
+
+// Generate a star <div>
+
+const genStar = () => {
+    const star = document.createElement("div");
+    star.classList.add("star");
+
+    // Gen star coordinates relative to $el size
+    let x = genRandomNumber(1, $el.offsetWidth);
+    let y = genRandomNumber(1, $el.offsetHeight);
+
+    const { style } = star;
+    
+    style.left = Math.floor(x) + "px";
+    style.top = Math.floor(y) + "px";
+
+    style.setProperty(
+        "--star-size",
+        genRandomNumber(1, 3) + "px"
+    );
+
+    style.setProperty(
+        "--twinkle-duration",
+        Math.ceil(genRandomNumber(1, 5)) + "s"
+    );
+
+    style.setProperty(
+        "--twinkle-delay",
+        Math.ceil(genRandomNumber(1, 5)) + "s"
+    );
+
+    return star;
+}
+
+for (let index = 0; index < 800; index++) {
+    $el.append(genStar());
+}
